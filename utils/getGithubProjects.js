860db7432067016de287ae76dadd1d1890274a89
Eber <52986565+doneber@users.noreply.github.com>
2023-03-05 10:51:38 -0400

    Update fetch gh pinned projects

--- utils/getGithubProjects.js
+++ utils/getGithubProjects.js
@@ -2,10 +2,11 @@ import puppeteer from 'puppeteer'
 import path from 'path'
 import { cwd } from 'node:process'
 import fetch from 'node-fetch'
+import dotenv from 'dotenv'
 import fs from 'node:fs'
 
 (async () => {
-
+  dotenv.config()
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
 
@@ -13,18 +14,44 @@ import fs from 'node:fs'
   await page.setViewport({ width: 1280, height: 800 })
 
   const username = "doneber"
-  const url = `https://api.github.com/users/${username}/repos?sort=pushed&direction=desc&per_page=5`
-  const githubProjects = await fetch(url).then((res) => res.json())
+  const endpoint = 'https://api.github.com/graphql';
+  const query = `{
+  user(login: "${username}") {
+    pinnedItems(first: 6, types: REPOSITORY) {
+      nodes {
+        ... on Repository {
+          name
+          description
+          repo_url: url
+          homepage_url: homepageUrl
+        }
+      }
+    }
+  }
+}`;
+
+  const headers = {
+    'Authorization': `Bearer ${process.env.GH_TOKEN_ACCESS}`,
+    'Content-Type': 'application/json'
+  };
+
+  const githubProjects = await fetch(endpoint, {
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify({ query: query })
+  })
+    .then(response => response.json())
+    .then(data => data['data']['user']['pinnedItems']['nodes'])
+    .catch(error => console.error(error));
+    console.log(githubProjects)
+    console.log(typeof githubProjects)
+    
   const projectsData = githubProjects.map((project) => {
     return {
-      name: project.name,
-      description: project.description,
-      repo_url: project.html_url,
-      homepage_url: project.homepage,
-      image: `${project.name}.jpg`,
+      ...project,
+      image: `${project.name}.jpg`
     }
   })
-
   // Save the data in a json file
   const jsonString = JSON.stringify({ projects: projectsData })
   fs.writeFile(path.join(cwd(), 'gh_projects_data', 'projects.json'), jsonString, (err) => {
