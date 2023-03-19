ce797ae86f08fdee39df311049ae986dda363fd0
Eber <52986565+doneber@users.noreply.github.com>
2023-03-18 20:42:47 -0400

    Add i18n and update details

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,10 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 import tailwind from "@astrojs/tailwind";
+import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-modern-personal-website.netlify.app',
-  integrations: [mdx(), sitemap(), tailwind()]
+  site: 'https://doneber.dev',
+  integrations: [mdx(), sitemap(), tailwind(), astroI18next()]
 });
