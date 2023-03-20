44bacda5abef4c7d0420ccfd36a81159bd46ed6c
Eber <52986565+doneber@users.noreply.github.com>
2023-03-20 19:03:20 -0400

    Improve performance and accessibility

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	daisyui: {
-    themes: ["dark", "light"],
-  },
+		themes: [
+			"light",
+			{
+				"dark": {
+					...require("daisyui/src/colors/themes")["[data-theme=dark]"],
+					"neutral-content": "#d5d8de",
+					"base-content": "#d5d8de",
+				}
+			}
+		],
+	},
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
 	},
-	plugins: [require("@tailwindcss/typography"),require("daisyui")],
+	plugins: [require("@tailwindcss/typography"), require("daisyui")],
 }
